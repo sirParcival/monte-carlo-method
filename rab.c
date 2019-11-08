b9c3b1ee5dd6da762cc017de7a3affce567fd400
Ruslan Kossak <r.kossak@stud.elka.pw.edu.pl>
2019-11-08 11:10:53 +0100

    Added pairing rabbits

--- rab.c
+++ rab.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 #include "structure.h"
 
 void add_rabbits(rabbit **r, int id);
@@ -8,18 +8,19 @@ void print_rabbits(rabbit *r);
 void free_rabbits(rabbit *r);
 void delete_rabbits(rabbit *r);
 void count_rabbits(rabbit *r, unsigned int age);
+void build_pairs(rabbit *r);
 
 int main(void)
 {
-    
-
     rabbit *rabbits = NULL;
+
     for(int i = 1; i <= 9600; i++)
     {
         add_rabbits(&rabbits, i);
     }
     count_rabbits(rabbits, 1);
     delete_rabbits(rabbits);
+    build_pairs(rabbits);
     print_rabbits(rabbits);
     free_rabbits(rabbits);
     return 0;
@@ -28,6 +29,10 @@ int main(void)
 void add_rabbits(rabbit **r, int id)
 {
     rabbit *r_list = malloc(sizeof(rabbit));
+    if(!r_list)
+    {
+        return -1;
+    }
     rabbit *last = *r;
     r_list->id = id;
     r_list->next = NULL;
@@ -55,12 +60,11 @@ void print_rabbits(rabbit *r)
     rabbit *first;
     while (r != NULL)
     {
-        fprintf(outfile,"%d, %d\n", r->id, r->age);
+        fprintf(outfile,"%d, %d, %d, %d, %d\n", r->id, r->age, r->adult, r->paired, r->pair_id);
         first = r;
         r = r->next;
     }
     fclose(outfile);
-
 }
 
 void count_rabbits(rabbit *r, unsigned int age)
@@ -69,14 +73,55 @@ void count_rabbits(rabbit *r, unsigned int age)
     while (ptr != NULL)
     {
         ptr->age = age;
+        ptr->adult = false;
+        ptr->paired = false;
         rabbit *next = ptr->next;
         if (ptr->id % 40 == 0)
         {
             age++;
         }
+        if (ptr->age >= 12)
+        {
+            ptr->adult = true;
+        }
+        
         ptr = next;
     }
 }
+
+void build_pairs(rabbit *r)
+{
+    rabbit *ptr = r;
+    while (ptr != NULL)
+    {
+        rabbit *next = ptr->next;
+        if (ptr->adult && !ptr->paired)
+        {
+            if (ptr->next)
+            {
+                rabbit *tmp = ptr->next;
+                rabbit *tmp_next = ptr->next->next;
+                while (tmp != NULL && tmp->next != NULL)
+                {
+                    if (tmp->adult && !tmp->paired)
+                    {
+                        ptr->paired = true;
+                        tmp->paired = true;
+                        tmp->pair_id = ptr->id;
+                        ptr->pair_id = tmp->id;
+                        break;
+                    }
+                tmp = tmp_next;
+                tmp_next = tmp_next->next;
+                
+                }
+            }     
+        }
+        ptr = next;
+    }
+    
+}
+
 
 void delete_rabbits(rabbit *r)
 {
--- structure.h
+++ structure.h
@@ -1,9 +1,13 @@
+#include <stdbool.h>
+
 typedef struct rabbit
 {
     struct rabbit *prev;
     unsigned int id;
     unsigned int age;
-   
-    
+    int pair_id;
+    bool adult;
+    bool paired;
+
     struct rabbit *next;
 }rabbit;
